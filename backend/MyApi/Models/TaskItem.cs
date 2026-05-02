156c0bfa766b09d8a2bd3b733e5da877fa400fc8
KN5YJF <kn5yjf@neptun.gde.hu>
2026-05-02 13:59:21 +0200

    Upgrade project: CI/CD, Docker, backend validation, Kubernetes and ArgoCD

--- backend/MyApi/Models/TaskItem.cs
+++ backend/MyApi/Models/TaskItem.cs
@@ -1,14 +1,18 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyApi.Models;
 
 public class TaskItem
 {
     [BsonId]
-[BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
+    [Required]
+    [MinLength(1)]
+    [StringLength(120)]
     public string Title { get; set; } = null!;
     public bool IsCompleted { get; set; } = false;
 }
